590b9da5cf11f9fe164e2fa80192cbe00d305f28
pangestukumoro <35019083+pangestukumoro@users.noreply.github.com>
2018-04-30 14:49:26 +0700

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,97 @@
+class Queue {
+	static int ekor=-1;
+	static int maxEkor=9;
+
+	public static void addQueue(String antrian[], String data) {
+		if(ekor>=maxEkor) {
+			System.out.println("Antrian penuh");
+		}
+		else {
+			System.out.println("addQueue : " +data);
+			ekor++;
+			antrian[ekor]=data;
+		}
+	}
+
+	public static String deQueue(String antrian[]) {
+		String hasil=" ";
+		if(ekor<0) {
+			hasil="Antrian kosong";
+		}
+		else {
+			hasil=antrian[0];
+			antrian[0]=null;
+
+			for(int i=1; i<=ekor; i++) {
+				antrian[i-1]=antrian[i];
+				antrian[i]=null;
+			}
+			ekor--;
+		}
+		return (hasil);
+	}
+
+	public static void bacaAntrian (String antrian[]) {
+		System.out.println("Kondisi antrian : ");
+		for(int i=0; i<=maxEkor; i++) {
+			if (i==ekor)
+				System.out.println((i+1) +"." +antrian[i] +" ==> ekor");
+			else
+				System.out.println((i+1) +"." +antrian[i]);
+		}
+	}
+
+	public static void clear(String antrian[]) {
+		for(int i=maxEkor; i>=0; i--) {
+			String hasil=" ";
+			if(ekor<0) {
+				hasil="Antrian kosong";
+			}
+			else {
+				hasil=antrian[0];
+				antrian[0]=null;
+
+				for(int j=1; j<=ekor; j++) {
+					antrian[i-1]=antrian[i];
+					antrian[i]=null;
+				}
+			}
+			ekor--;
+		}
+	}
+
+	public static void main(String[] args) {
+		String antrian[]=new String[100];
+		addQueue(antrian, "Mobil A");
+		addQueue(antrian, "Mobil B");
+		addQueue(antrian, "Mobil C");
+		addQueue(antrian, "Mobil D");
+		System.out.println();
+		bacaAntrian(antrian);
+		System.out.println();
+
+		addQueue(antrian, "Mobil E");
+		addQueue(antrian, "Mobil F");
+		addQueue(antrian, "Mobil G");
+		System.out.println();
+		bacaAntrian(antrian);
+		System.out.println();
+
+		addQueue(antrian, "Mobil H");
+		addQueue(antrian, "Mobil I");
+		addQueue(antrian, "Mobil J");
+		addQueue(antrian, "Mobil K");
+		addQueue(antrian, "Mobil L");
+		System.out.println();
+		bacaAntrian(antrian);
+		System.out.println();
+
+		System.out.println("deQueue : " +deQueue(antrian));
+		bacaAntrian(antrian);
+
+		clear(antrian);
+		System.out.println();
+		System.out.println("Clear!");
+		bacaAntrian(antrian);
+	}
+}
